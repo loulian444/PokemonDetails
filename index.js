59b66fbb981f2f60872856b72cb7e9b49a7d4644
Loulian Liu <3l.liu444@gmail.com>
2023-07-14 15:33:15 -0400

    commit with the weird a tag situation

--- index.js
+++ index.js
@@ -1,34 +1,20 @@
 console.log(`hehehoohoohaha`);
 
+let pokeList = [];
 const get20Pokes = async () => {
   try {
     const response = await fetch(`https://pokeapi.co/api/v2/pokemon/`);
     const data = await response.json();
-    const pokeRes = data.results;
-
-    return pokeRes;
+    return data.results;    
   } catch (err) {
     console.log(err);
   }
 };
 
-const getPokeID = (pokeResArr) => {
-  const pokeNameID = pokeResArr.map((singlePoke) => {
-    return {
-      name: singlePoke.name,
-      id: singlePoke.url.split(`/`)[6],
-    };
-  });
-
-  return pokeNameID;
-};
-
-const getPokeInfo = async (pokeNameID) => {
+const getPokeInfo = async (pokemonList) => {
   const pokeInfo = [];
-  for (let i = 0; i < pokeNameID.length; i++) {
-    const response = await fetch(
-      `https://pokeapi.co/api/v2/pokemon/${pokeNameID[i].id}`
-    );
+  for (let i = 0; i < pokemonList.length; i++) {
+    const response = await fetch(pokemonList[i].url);
     const data = await response.json();
 
     pokeInfo.push(data);
@@ -40,32 +26,42 @@ const getPokeInfo = async (pokeNameID) => {
 const getPokeSprite = (pokeInformation) => {
   const pokeSprites = pokeInformation.map(
     (singlePoke) => singlePoke.sprites.front_default
-  );
-  
+    );
+    
   return pokeSprites
 };
 
 const putSpritesToImg = (spritesArr) => {
   const imgSpriteArr = spritesArr.map((spriteURL) => {
-    return `<img src="${spriteURL}"/>`
+    const img = document.createElement(`img`);
+    img.src = spriteURL;
+    
+    return img;
   })
 
   return imgSpriteArr;
 };
 
 const render = (sprites) => {
-  const ul = document.querySelector(`ul`)
-  ul.innerHTML = sprites.join(``);
+  const section = document.querySelector(`section`);
+  sprites.forEach((sprite) => {
+    section.appendChild(sprite);
+  });
 }
 
-const getImgLink = (spritesImg, pokemonID) => {
-  const spriteLink = [];
-  for (let i = 0; i < spritesImg.length; i++) {
-    const imgLink = `<a href="" data-number="${pokemonID[i].id}">${spritesImg[i]}</a>`;
-    spriteLink.push(imgLink);
+const getImgLink = (sprites, pokemonInfo) => {
+  const imgLink = [];
+  for (let i = 0; i < sprites.length; i++) {
+    const a = document.createElement(`a`);
+    a.setAttribute(`data-number`, pokemonInfo[i].id);
+    a.appendChild(sprites[i]);
+    console.log(a)
+
+    imgLink.push(a);
+    // const imgLink = `<a href="" data-number="${pokemonInfo[i].id}">${spritesImg[i]}</a>`;
   }
 
-  return spriteLink;
+  return imgLink;
 }
 
 const addLinkEventListeners = () => {
@@ -94,12 +90,12 @@ const renderPokeDetails = (pokeData) => {
 }
 
 const init = async () => {
-  const allPokeRes = await get20Pokes();
-  const allPokeNameID = getPokeID(allPokeRes);
-  const allPokeInfo = await getPokeInfo(allPokeNameID);
+  pokeList = await get20Pokes();
+  const allPokeInfo = await getPokeInfo(pokeList);
   const allPokeSprites = getPokeSprite(allPokeInfo);
   const allImgSprites = putSpritesToImg(allPokeSprites);
-  const allImgLink = getImgLink(allImgSprites, allPokeNameID);
+  const allImgLink = getImgLink(allImgSprites, allPokeInfo);
+  // console.log(allImgLink)
   render(allImgLink);
   addLinkEventListeners();
 };
